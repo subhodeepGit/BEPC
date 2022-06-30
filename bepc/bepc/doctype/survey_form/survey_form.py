b04cb588d99503f3b7c54578ba7fe44d86a38216
Rupalib-soul <rupali.bhatta@soulunileaders.com>
2022-06-30 15:58:42 +0530

    Workflow ,Survey form and template added

--- bepc/bepc/doctype/survey_form/survey_form.py
+++ bepc/bepc/doctype/survey_form/survey_form.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+from os import dup
+from warnings import filters
+import frappe
+from frappe.model.db_query import get_date_range
 from frappe.model.document import Document
+from frappe.utils.data import getdate
 
 class SurveyForm(Document):
-	pass
+	def validate(self):
+		self.validate_dates()
+		
+	def validate_dates(self):
+		if self.date_of_upload and getdate(self.date_of_upload) > getdate():
+			frappe.throw(frappe._("Date of Upload cannot be greater than today's date."))
+
+	print("\n\n\n\n\n\n")
+	print(frappe.session.user)
+
