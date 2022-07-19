717ee6588c1cda53c698bb10aa527071576fccbe
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-07-19 16:45:36 +0530

    theft case doctype and child doctype added

--- bepc/bepc/doctype/theft_case_support_documents/test_theft_case_support_documents.py
+++ bepc/bepc/doctype/theft_case_support_documents/test_theft_case_support_documents.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestTheftCasemonitoring(unittest.TestCase):
+class TestTheftCaseSupportDocuments(unittest.TestCase):
 	pass
--- bepc/bepc/doctype/theft_case_support_documents/theft_case_support_documents.js
+++ bepc/bepc/doctype/theft_case_support_documents/theft_case_support_documents.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, SOUL and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Theft Case monitoring', {
+frappe.ui.form.on('Theft Case Support Documents', {
 	// refresh: function(frm) {
 
 	// }
