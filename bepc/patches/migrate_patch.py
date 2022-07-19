4afdf8088254edf6034d1c2e1f63f044a5891271
Shruti-Soul <shruti.kumari@soulunileaders.com>
2022-07-19 16:24:55 +0530

    Support  Role Permission

--- bepc/patches/migrate_patch.py
+++ bepc/patches/migrate_patch.py
@@ -21,3 +21,19 @@ def add_module_profile():
                 for k in d.keys():
                     mp.set(k,d.get(k))
                 mp.save()
+
+def get_custom_role_permission(site=None):
+    if sys.argv[2]=='--site':
+        os.system("bench --site {0} export-fixtures".format(sys.argv[3]))
+    else:
+        os.system("bench export-fixtures")
+
+#   bench execute ed_tec.patches.migrate_patch.set_custom_role_permission
+def set_custom_role_permission():
+    with open(frappe.get_app_path("bepc","fixtures","custom_docperm.json")) as f:
+        for d in json.load(f):
+            if len(frappe.get_all('Custom DocPerm',{'parent':d.get('parent'),'role':d.get('role')}))==0:
+                role=frappe.new_doc('Custom DocPerm')
+                for k in d.keys():
+                    role.set(k,d.get(k))
+                role.save()                
