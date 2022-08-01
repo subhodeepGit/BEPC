729068637ae5d621fdef81b4bc0b79fbf254317f
SPriytesh <priytesh.shah@soulunileaders.com>
2022-08-01 18:45:33 +0530

    Dashboard

--- bepc/bepc/doctype/student_entry/student_entry.js
+++ bepc/bepc/doctype/student_entry/student_entry.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student Entry', {
-	// refresh: function(frm) {
-
-	// }
+		final_score:function(frm, cdt, cdn){
+		print("hello")
+		var d = locals[cdt][cdn];
+		var total = 0;
+		let a= parseInt(total)
+		frm.doc.total_student.forEach(function(d)  { a = a+ d.final_score; });
+		frm.set_value("total_student_per_school", a);
+		refresh_field("total_student_per_school");
+	  },
 });
+frappe.ui.form.on('Class', {
+	total_student:function(frm, cdt, cdn){
+	var d = locals[cdt][cdn];
+	var total = 0;
+	let a= parseInt(total)
+	frm.doc.class.forEach(function(d)  { a = a+ d.total_student; });
+	frm.set_value("total_student_per_school", a);
+	refresh_field("total_student_per_school");
+  },
+  class_remove:function(frm, cdt, cdn){
+	var d = locals[cdt][cdn];
+	var total = 0;
+	let a= parseInt(total)
+	frm.doc.class.forEach(function(d) { a += d.total_student; });
+	frm.set_value("total_student_per_school", a);
+	refresh_field("total_student_per_school");
+		}
+	});
