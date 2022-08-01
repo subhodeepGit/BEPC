fc3a919b9d43b78acce80634838b3b4fa0c850f3
SPriytesh <priytesh.shah@soulunileaders.com>
2022-08-01 16:24:12 +0530

    Dashboard,Repor,tValidation and some chnages

--- bepc/bepc/doctype/class/class.py
+++ bepc/bepc/doctype/class/class.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class Site(Document):
+class Class(Document):
 	pass
--- bepc/bepc/doctype/student_entry/test_student_entry.py
+++ bepc/bepc/doctype/student_entry/test_student_entry.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestSite(unittest.TestCase):
+class TestStudentEntry(unittest.TestCase):
 	pass
