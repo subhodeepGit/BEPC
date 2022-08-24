fa7bac43b1f08415c0328c9989a4d28751e02b79
SPriytesh <priytesh.shah@soulunileaders.com>
2022-08-24 12:18:14 +0530

    removed phone no valid from school & survey frm

--- bepc/bepc/doctype/school/school.py
+++ bepc/bepc/doctype/school/school.py
@@ -6,9 +6,9 @@ from frappe.model.document import Document
 
 class School(Document):
 	def validate(doc):
-		mobile_number_validation(doc)
-		mobile_number(doc)
-		number_validation(doc)
+		# mobile_number_validation(doc)
+		# mobile_number(doc)
+		# number_validation(doc)
 		pincode(doc)
 
 def pincode(doc):
@@ -20,29 +20,29 @@ def pincode(doc):
 		if len(doc.pin_code)<6:
 			frappe.throw("Field <b>Pin Code</b> must be 6 Digits")
 
-def mobile_number_validation(doc):
-	if doc.school_contact_no:
-		if not (doc.school_contact_no).isdigit():
-			frappe.throw("Field <b>School Contact Number</b> Accept Digits Only")
-		if len(doc.school_contact_no)>10:
-			frappe.throw("Field <b>School Contact Number</b> must be 10 Digits")
-		if len(doc.school_contact_no)<10:
-			frappe.throw("Field <b>School Contact Number</b> must be 10 Digits")
+# def mobile_number_validation(doc):
+# 	if doc.school_contact_no:
+# 		if not (doc.school_contact_no).isdigit():
+# 			frappe.throw("Field <b>School Contact Number</b> Accept Digits Only")
+# 		if len(doc.school_contact_no)>10:
+# 			frappe.throw("Field <b>School Contact Number</b> must be 10 Digits")
+# 		if len(doc.school_contact_no)<10:
+# 			frappe.throw("Field <b>School Contact Number</b> must be 10 Digits")
 
-def mobile_number(doc):
-	if doc.principle_mobile_number:
-		if not (doc.principle_mobile_number).isdigit():
-			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> Accept Digits Only")
-		if len(doc.principle_mobile_number)>10:
-			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> must be 10 Digits")
-		if len(doc.principle_mobile_number)<10:
-			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> must be 10 Digits")
+# def mobile_number(doc):
+# 	if doc.principle_mobile_number:
+# 		if not (doc.principle_mobile_number).isdigit():
+# 			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> Accept Digits Only")
+# 		if len(doc.principle_mobile_number)>10:
+# 			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> must be 10 Digits")
+# 		if len(doc.principle_mobile_number)<10:
+# 			frappe.throw("Field <b>Headmaster/Head Mistress Contact No</b> must be 10 Digits")
 
-def number_validation(doc):
-	if doc.president_contact_no:
-		if not (doc.president_contact_no).isdigit():
-			frappe.throw("Field <b>President Contact Number</b> Accept Digits Only")
-		if len(doc.president_contact_no)>10:
-			frappe.throw("Field <b>President Contact Number</b> must be 10 Digits")
-		if len(doc.president_contact_no)<10:
-			frappe.throw("Field <b>President Contact Number</b> must be 10 Digits")
+# def number_validation(doc):
+# 	if doc.president_contact_no:
+# 		if not (doc.president_contact_no).isdigit():
+# 			frappe.throw("Field <b>President Contact Number</b> Accept Digits Only")
+# 		if len(doc.president_contact_no)>10:
+# 			frappe.throw("Field <b>President Contact Number</b> must be 10 Digits")
+# 		if len(doc.president_contact_no)<10:
+# 			frappe.throw("Field <b>President Contact Number</b> must be 10 Digits")
