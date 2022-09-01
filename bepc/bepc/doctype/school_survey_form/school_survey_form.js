dbba4f7dd20fc070a11b12225431504de28b263d
SPriytesh <priytesh.shah@soulunileaders.com>
2022-09-01 13:01:45 +0530

    removed year of start validation

--- bepc/bepc/doctype/school_survey_form/school_survey_form.js
+++ bepc/bepc/doctype/school_survey_form/school_survey_form.js
@@ -1,12 +1,12 @@
 // Copyright (c) 2022, SOUL and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('School Survey Form', "validate", function(frm) {
-        if (frm.doc.year_of_start__project > get_today()) {
-            frappe.msgprint(__("You can not select future date in Year of Start Project"));
-            frappe.validated = false;
-        }
-});
+// frappe.ui.form.on('School Survey Form', "validate", function(frm) {
+//         if (frm.doc.year_of_start__project > get_today()) {
+//             frappe.msgprint(__("You can not select future date in Year of Start Project"));
+//             frappe.validated = false;
+//         }
+// });
 
 frappe.ui.form.on('School Survey Form', 'onload', function(frm) {
     frappe.call({
