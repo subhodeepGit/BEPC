20c4018c4a24f0e7f05f844e1c406a6119a7014d
SPriytesh <priytesh.shah@soulunileaders.com>
2023-09-26 11:04:21 +0530

    New doctype for Childrens, number cards and new dashboard

---
+++ bepc/bepc/doctype/total_enrolled_children/total_enrolled_children.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, SOUL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Total Enrolled Children', {
+	// refresh: function(frm) {
+
+	// }
+});
