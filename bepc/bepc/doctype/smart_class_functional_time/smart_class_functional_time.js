a9b06b65c08a9a1836f0f3b02031ecfca38df0af
SPriytesh <priytesh.shah@soulunileaders.com>
2023-09-28 17:44:47 +0530

    Doctypes created to store data from azure database

---
+++ bepc/bepc/doctype/smart_class_functional_time/smart_class_functional_time.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, SOUL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Smart Class Functional Time', {
+	// refresh: function(frm) {
+
+	// }
+});
